67d73b6355c9be915a3e9d06493c1ef2b1fcdc8f
Daniil_E <danny190100@gmail.com>
2020-03-27 14:44:42 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);//не доконца понял зачем это обьявлять?
+        System.out.println("Whats your name?");
+        String name = scanner.nextLine();
+        System.out.println("Where are you live, " + name + "?");
+        String spc = scanner.nextLine();
+        System.out.println("Name:" + name + ". Adress:"+ spc);
+    }
+}
